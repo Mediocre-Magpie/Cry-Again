a208cebbfafbfa0e0ddea79247ca80cf1903631b
Matt <1024280@student.sae.edu.au>
2022-07-29 15:41:12 +0800

    Matt Minigames Import

---
+++ Assets/Matts Minigames/Rhythm Game/Scripts/GameManagerMatt.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerMatt : MonoBehaviour
+{
+
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public ArrowScroller theAS;
+
+    public static GameManagerMatt instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theAS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit the Note");
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed the Note");
+    }
+}
